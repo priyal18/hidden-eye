8bec71a13cc0d19b04add12855044274c015e1a1
priyal18 <priyal930gupta@gmail.com>
2020-09-01 16:10:08 +0530

    homePage

--- realsecurity/urls.py
+++ realsecurity/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('home.html', views.home,name='homepage'),
     path('Services.html', views.Services,name='Services'),
     path('Contact.html', views.Contact,name='Contact'),
-    path('pawn_check.html', views.pawn_check,name='Pawn Checking')
+    path('pawn_check.html', views.pawn_check,name='Pawn Checking'),
+    path('pawn_user', views.pawn_user,name='Checking username'),
+    path('pawn_pass', views.pawn_pass,name='Checking password')
+
+
 ]
--- realsecurity/views.py
+++ realsecurity/views.py
@@ -9,3 +9,26 @@ def Contact(request):
 	return render(request,'realsecurity/Contact.html')
 def pawn_check(request):
 	return render(request,'realsecurity/pawn_check.html')
+def pawn_user(request):
+	username=request.POST["username"]
+	username+='\n'
+	result=""
+	with open('/usr/share/wordlists/rockyou.txt','r') as f:
+		for line in f:
+			if line==username:
+				result="Oh no - Pwned!!!"
+				return render(request,'realsecurity/result.html',{'result':result})
+		result="Good news — no pwnage found"
+		return render(request,'realsecurity/result.html',{'result':result})
+
+def pawn_pass(request):
+	password=request.POST["password"]
+	password+='\n'
+	result=""
+	with open('/usr/share/wordlists/rockyou.txt','r') as f:
+		for line in f:
+			if line==password:
+				result="Oh no - Pwned!!!"
+				return render(request,'realsecurity/result.html',{'result':result})
+		result="Good news — no pwnage found"
+		return render(request,'realsecurity/result.html',{'result':result})
