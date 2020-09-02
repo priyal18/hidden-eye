91275b6666408bde745acd4f3ed9ca2edd7f8c48
priyal18 <priyal930gupta@gmail.com>
2020-09-02 11:18:47 +0530

    Vulnerability Check , Pawn Check

--- realsecurity/urls.py
+++ realsecurity/urls.py
@@ -3,11 +3,12 @@ from . import views
 urlpatterns = [
     path('', views.home,name='homepage'),
     path('home.html', views.home,name='homepage'),
-    path('pawn_check.html', views.Services,name='Services'),
+    path('Services.html', views.Services,name='Services'),
     path('Contact.html', views.Contact,name='Contact'),
     path('pawn_check.html', views.pawn_check,name='Pawn Checking'),
     path('pawn_user', views.pawn_user,name='Checking username'),
-    path('pawn_pass', views.pawn_pass,name='Checking password')
+    path('pawn_pass', views.pawn_pass,name='Checking password'),
+    path('vulnerability_check.html',views.vulnerability_check,name="Vulnerability Check")
 
 
 ]
--- realsecurity/views.py
+++ realsecurity/views.py
@@ -5,31 +5,41 @@ from django.views.decorators.csrf import csrf_exempt
 def home(request):
 	return render(request,'realsecurity/home.html')
 def Services(request):
-	return render(request,'realsecurity/pawn_check.html')
+	return render(request,'realsecurity/Services.html')
 def Contact(request):
 	return render(request,'realsecurity/Contact.html')
 def pawn_check(request):
 	return render(request,'realsecurity/pawn_check.html')
+def vulnerability_check(request):
+	return render(request,'realsecurity/vulnerability_check.html')
 
 @csrf_exempt
 def pawn_user(request):
 	username=request.POST["username"]
+	if(username==""):
+	
+		result="Seems like you forgot to enter a username!"
+		return render(request,'realsecurity/pawn_check.html',{'result':result})
+	
 	username+='\n'
 	result=""
-	with open('/usr/share/wordlists/rockyou.txt','r') as f:
+	with open('/home/priyal/Desktop/word_list.txt','r') as f:
 		for line in f:
 			if line==username:
-				result="Oh no - Pwned!!!"
+				result="Oh no - You have been Pwned!!! :("
 				return render(request,'realsecurity/pawn_check.html',{'result':result})
-		result="Good news — no pwnage found"
+		result="Good news — no pwnage found :)"
 		return render(request,'realsecurity/pawn_check.html',{'result':result})
 
 @csrf_exempt
 def pawn_pass(request):
 	password=request.POST["password"]
+	if(password==""):
+		result="Seems like you forgot to enter a Password!"
+		return render(request,'realsecurity/pawn_check.html',{'result':result})
 	password+='\n'
 	result=""
-	with open('/usr/share/wordlists/rockyou.txt','r') as f:
+	with open('/home/priyal/Desktop/word_list.txt','r') as f:
 		for line in f:
 			if line==password:
 				result="Oh no - Pwned!!!"
